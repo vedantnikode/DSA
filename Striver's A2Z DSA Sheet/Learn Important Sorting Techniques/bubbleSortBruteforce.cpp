1dc9e653db3678490b5908b71c5bd94d57dbac16
vedantnikode <vedantnikode1@gmail.com>
2026-06-21 18:40:24 +0530

    add bubble sorting by brutal aproach

---
+++ Striver's A2Z DSA Sheet/Learn Important Sorting Techniques/bubbleSortBruteforce.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void BubbleSort(int arr[], int n){
+    for (int i=n-1; i>=0; i--){
+        for(int j=0; j<i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }   
+        }
+    }
+
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    BubbleSort(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
