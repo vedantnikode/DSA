596f4d9ad34eaf47ec261a6a5b7366ac532175ea
vedantnikode <vedantnikode1@gmail.com>
2026-06-23 13:24:48 +0530

    added optimal approach for largest Element

--- Striver's A2Z DSA Sheet/Problems on Arrays/Easy/largestElement.cpp
+++ Striver's A2Z DSA Sheet/Problems on Arrays/Easy/largestElement.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+int LargestEle(int arr[], int n){
+    int maxEle = arr[0];
+    for (int i=0; i<n; i++){
+        if (arr[i]>maxEle){
+            maxEle = arr[i];
+        }
+    }
+
+    return maxEle ;
+}
+
+
+
+
+int main(){
+    int arr[]= {1,2,3,4,3,2,2,1,7,8,9,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<LargestEle(arr, n);
+    return 0;
+
+}
