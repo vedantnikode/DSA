9a0a196be82f98b76f0f792b4a6e3771ad3c49a5
vedantnikode <vedantnikode1@gmail.com>
2026-07-02 22:48:05 +0530

    added brute force of removing duplicates

---
+++ Striver's A2Z DSA Sheet/Problems on Arrays/Easy/Remove Duplicates/bruteforce.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution{
+public:
+    int removeDuplicate(vector<int>& arr){
+        int i=0;
+        for(int j=1; j<arr.size(); j++){
+            if (arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i;
+    }
+};
+
+int main(){
+    vector<int> arr = {1,2,3,3,4,5,5,6};
+    Solution obj;
+    int k = obj.removeDuplicate(arr);
+
+    for(int i=0; i<=k; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+      
+}
