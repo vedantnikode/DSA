456102e256a5c7cb3d6a0c93331a677f66362ec8
vedantnikode <vedantnikode1@gmail.com>
2026-07-08 20:04:55 +0530

    Union of array using map

---
+++ Striver's A2Z DSA Sheet/Problems on Arrays/Easy/Union of two sorted arrays/approach1usingmap.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> FindUnion(int arr1[], int arr2[], int n, int m){
+        map<int,int> freq;
+        for(int i=0; i<n; i++){
+            freq[arr1[i]]++;
+        }
+        for(int i=0; i<m; i++){
+            freq[arr2[i]]++;
+        }
+
+        vector<int> Union;
+        for(auto& it : freq){
+            Union.push_back(it.first);
+        }
+        return Union;
+    }
+
+};
+
+int main(){
+    int arr1[] = {1,2,3,3,4,4,5,6,7,8};
+    int arr2[] = {3,4,5,5,6,6,7,8,9,10,11,11};
+    int n = sizeof(arr1)/sizeof(arr1[0]);
+    int m = sizeof(arr2)/sizeof(arr2[0]);
+
+    Solution obj;
+    vector<int> Union = obj.FindUnion(arr1, arr2, n, m);
+
+    for(auto& ints : Union){
+        cout<<ints<<" ";
+    }
+}
