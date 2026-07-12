8b41e60d8ef39e619ac82fffc2c54e945b099530
vedantnikode <vedantnikode1@gmail.com>
2026-07-12 22:59:17 +0530

    halfway of longest subarray

---
+++ Striver's A2Z DSA Sheet/Problems on Arrays/Easy/Longest Subarray with given Sum K(Positives)/bruteforce.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+public:
+    int LongestSubarray(vector<int> arr, int k){
+
+    }
+};
+
+int main(){
+    vector<int> arr = {10, 5, 2, 7, 1, 9};
+    int k = 15;
+    Solution obj;
+    cout<<obj.LongestSubarray(arr, k);
+    return 0;
+}
