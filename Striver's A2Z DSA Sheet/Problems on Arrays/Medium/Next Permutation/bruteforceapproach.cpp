c4437f638c828b18b960b7c3a65cda9f80daf777
vedantnikode <vedantnikode1@gmail.com>
2026-08-11 21:35:21 +0530

    added complete bruteforce for nextpermutation

--- Striver's A2Z DSA Sheet/Problems on Arrays/Medium/Next Permutation/bruteforceapproach.cpp
+++ Striver's A2Z DSA Sheet/Problems on Arrays/Medium/Next Permutation/bruteforceapproach.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution{
 public:
     vector<int> nextPermutation(vector<int> nums){
+        vector<vector<int>> all;
+        sort(nums.begin(), nums.end());
+
+        do{
+            all.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        
+        for(int i=0; i<all.size(); i++){
+            if(all[i]==nums){
+                if(i==all.size()-1){
+                    return all[0];
+                }
+                return all[i+1];
+            }
+            
+
+        }
+        
         
 
 
